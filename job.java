853545f3d5922afa99c79ebdd24688b4ca561f15
nrohith-git <miniprojectcse000@gmail.com>
2026-04-08 14:37:55 +0530

    Create job.java

---
+++ job.java
@@ -0,0 +1,9 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        int firstNum = 10;
+        int secondNum = 20;
+        int sum = firstNum + secondNum;
+        
+        System.out.println("The sum is: " + sum);
+    }
+}
